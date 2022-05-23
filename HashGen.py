66855236ed0b9f1c020833c2a513531081066c22
えびもなか <74030378+youse19999@users.noreply.github.com>
2022-05-23 12:26:41 +0900

    Add string2hash

--- HashGen.py
+++ HashGen.py
@@ -6,8 +6,11 @@ from hashlib  import blake2b
 def gethash(byte,size):
   hashgen = blake2b(byte,digest_size=size)
   return hashgen.hexdigest()
+  
+def string2hash(string,size):
+  return gethash(string.encode(),size)
 
 #example here
 print("Example hash¥ngive me name")
 gethas = input()
-print(gethash(gethas.encode(),8))
+print(string2hash(gethas,8))
